da1c9c609ccfa47be4f6d9d13c5e22199fef7d9d
CJ Yetman <cyetman@rmi.org>
2023-06-13 10:19:22 +0200

    adapt test to new data location

--- tests/testthat/test-translation_data.R
+++ tests/testthat/test-translation_data.R
@@ -3,9 +3,9 @@ test_that("translation data does not contain duplicate key rows", {
     testthat::skip("Not run in R CMD check")
   }
 
-  dataframe_headers <- readr::read_csv("../../data/translation/dataframe_headers.csv", col_types = "c")
-  dataframe_labels <- readr::read_csv("../../data/translation/dataframe_labels.csv", col_types = "c")
-  js_labels <- jsonlite::fromJSON("../../data/translation/js_labels.json")
+  dataframe_headers <- readr::read_csv("../../inst/extdata/translation/dataframe_headers.csv", col_types = "c")
+  dataframe_labels <- readr::read_csv("../../inst/extdata/translation/dataframe_labels.csv", col_types = "c")
+  js_labels <- jsonlite::fromJSON("../../inst/extdata/translation/js_labels.json")
 
   expect_false(any(duplicated(dataframe_headers[, c("id_data", "id_column")])))
   expect_false(any(duplicated(dataframe_labels[, c("id_data", "id_column", "key")])))
