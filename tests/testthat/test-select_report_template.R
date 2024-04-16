47c02be2f2ff052596b3eae9ca6358d9d4a28484
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-04-16 12:22:19 +0200

    Update tests/testthat/test-select_report_template.R
    Co-authored-by: CJ Yetman <cyetman@rmi.org>

--- tests/testthat/test-select_report_template.R
+++ tests/testthat/test-select_report_template.R
@@ -2,6 +2,6 @@ test_that("with no args, returns EN template string", {
   expect_equal(select_report_template(), "_en_template")
 })
 
-test_that("with fake args, returnsexpected string structure", {
+test_that("with fake args, returns expected string structure", {
   expect_equal(select_report_template("foo", "bar"), "foo_bar_template")
 })
