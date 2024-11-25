714bdbefc33e0b2c2549fe52a11cb082d4e2ac87
Alex Axthelm <aaxthelm@RMI.org>
2024-11-25 16:45:25 +0100

    Update and render documentation

--- R/choose_dictionary_language.R
+++ R/choose_dictionary_language.R
@@ -1,3 +1,26 @@
+#' Extract translation strings from translation data.frame
+#'
+#' Extract the translation strings from a translation data.frame for a given
+#' language.
+#'
+#' @param data data.frame containing translation data, with columns `id_data`,
+#' `id_column`, `key`, and one column for each language (lowercase ISO-639-1
+#' code). See examples for more detail.
+#' @param language ISO-639-1 code of the language to extract from the
+#' translation data.
+#' @return data.frame with tranlsated strings for the given language in the
+#' `translate_value` column.
+#' @examples
+# nolint start
+#' translation_data <- read.csv(
+#'   file = system.file(
+#'     "extdata", "translation", "dataframe_labels.csv",
+#'     package = "pacta.portfolio.report"
+#'   )
+#' )
+#' de_dictionary <- choose_dictionary_language(translation_data, "de")
+# nolint end
+#' @export
 choose_dictionary_language <- function(
   data,
   language
